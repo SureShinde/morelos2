0a1b1339866a0361dbe0413d253b9615982ced9f
jesus abelardo acevedo santos <laloas_2004@yahoo.com.mx>
2021-04-10 01:53:25 -0500

    se quitaron archivos de gitignore

---
+++ tienda/app/etc/env.php
@@ -0,0 +1,64 @@
+<?php
+
+return array (
+  'backend' => 
+  array (
+
+    'frontName' => 'admin',
+    
+  ),
+  'crypt' => 
+  array (
+    'key' => '7e724bd1d31575d0472331fa96b3f415',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' =>
+
+      array (
+        'host' => '127.0.0.1',
+        'dbname' => 'morelos',
+        'username' => 'morelos_user',
+        'password' => 'morelos1',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 03 Aug 2017 02:49:54 +0000',
+  ),
+);
